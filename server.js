fd60f1465eb25f3d558baa99d602bcf5bc2363d2
Justin <justinanderson101@gmail.com>
2021-04-04 01:14:35 +0000

    adding login and resource page

--- server.js
+++ server.js
@@ -50,6 +50,14 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/createresource", (req, res) => {
+  res.render("create_resource");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
