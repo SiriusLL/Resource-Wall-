f30c0a3b0f26c6a56e337ebb39317a2469a9933e
Sam T <bbrackish@protonmail.com>
2021-04-04 22:11:21 +0000

    add resource routes

--- routes/resource-router.js
+++ routes/resource-router.js
@@ -1,13 +1,14 @@
+const { response } = require('express');
 const express = require('express');
 const router  = express.Router();
 
-module.exports = (db) => {
+const resourceRoutes = (db) => {
   router.get("/", (req, res) => {
     let query = `SELECT * FROM resources`;
     console.log(query);
     db.query(query)
-      .then(data => {
-        const resources = data.rows;
+      .then(response => {
+        const resources = response.rows[0];
         res.json({ resources });
       })
       .catch(err => {
@@ -18,3 +19,5 @@ module.exports = (db) => {
   });
   return router;
 };
+
+module.exports = resourceRoutes;
