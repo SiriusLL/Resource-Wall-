80c3f1d1b6c9ab9e5fbb03859e2c63010cbb1660
Justin <justinanderson101@gmail.com>
2021-04-04 01:19:25 +0000

    editing error

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,8 +1,8 @@
 -- Users table seeds here (Example)
 INSERT INTO users (name, email, password)
  VALUES ('Justin Time', 'example@example.com', 'password'),
- ('Albert Einstein', 'eEqualsMC2.example.com', 'password'),
- ('Jean Sammet', 'formac.example.com', 'password'),
- ('Ron Swanson', 'breakfastChamp.example.com', 'password')
+ ('Albert Einstein', 'eEqualsMC2@example.com', 'password'),
+ ('Jean Sammet', 'formac@example.com', 'password'),
+ ('Ron Swanson', 'breakfastChamp@example.com', 'password')
  ;
 
