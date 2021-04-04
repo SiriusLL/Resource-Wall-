ef36d7988f408896b2b2e9ad6d011b52210bfb12
Sam T <bbrackish@protonmail.com>
2021-04-04 01:16:38 +0000

    Create seeds for Users and Url resources

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,2 +1,8 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, email, password) VALUES ('Justin', 'example@example.com', 'password');
+INSERT INTO users (name, email, password)
+ VALUES ('Justin Time', 'example@example.com', 'password'),
+ ('Albert Einstein', 'eEqualsMC2.example.com', 'password'),
+ ('Jean Sammet', 'formac.example.com', 'password'),
+ ('Ron Swanson', 'breakfastChamp.example.com', 'password')
+ ;
+
---
+++ db/seeds/02_urls.sql
@@ -0,0 +1,9 @@
+INSERT INTO resources (title, description, category, resource, user_id)
+ VALUES ('Heroic Programming', 'As long as the majority of programmers are willing to do stupid things to bail out their incompetent managers, we are all going to suffer.', 'Coding', 'https://wiki.c2.com/?HeroicProgramming', 2),
+ ('What shade of light grey can be seen on 99% of monitors?', 'Stack exchange question where a graphic designer gets information on accessibility.', 'Graphic Design', 'https://graphicdesign.stackexchange.com/questions/94401/what-shade-of-light-grey-can-be-seen-on-99-of-monitors', 2),
+ ('Landing page checklist', 'A page that has a checklist and resources for everystep in creating a new website', 'Web Dev', 'https://landingpage.fyi/landing-page-checklist.html', 3),
+ ('Starting Forth', 'An introduction to the Forth programming language.', 'Coding', 'https://www.forth.com/starting-forth/', 4),
+ ('SpatiaLite', 'SpatiaLite is an open source library intended to extend the SQLite core to support fully fledged Spatial SQL capabilities.', 'Coding', 'https://www.gaia-gis.it/fossil/libspatialite/index', 4),
+ ('Advanced programming in C', 'A guide to C language which is of choice for system programming, embedded systems and also a viable option for many other applications.', 'Coding', 'http://pfacka.binaryparadise.com/articles/guide-to-advanced-programming-in-C.html', 2),
+ ('HotPot', 'An api that helps non-designers create professional graphics and differs from other tools because we focus on challenges faced by developers and indie entrepreneurs.', 'Graphic Design', 'https://hotpot.ai/', 2)
+;
