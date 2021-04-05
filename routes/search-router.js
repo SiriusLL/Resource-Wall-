931449ea88fe7b9a63994c274ad8cdff302fac05
SiriusLL <sirius.love.light@gmail.com>
2021-04-05 12:17:21 +0000

    save

--- routes/search-router.js
+++ routes/search-router.js
@@ -6,11 +6,13 @@ const searchRoutes = (db) => {
   router.get("/", (req, res) => {
     let query = `
       SELECT * FROM resources
-      WHERE category LIKE %$1%
-      OR description LIKE %$2%;`;
+      WHERE category LIKE $1
+      OR description LIKE $2;`;
     console.log(query);
+    console.log(req.body, 'reqBooty@@@@@@@@@@@')
     db.query(query, [req.body, req.body])
       .then(response => {
+
         console.log(response.rows,
           'flag')
           templateVars = { resources: response.rows}
