776922e7b5a4762a7f7d184c271fb13899deb710
Sam T <bbrackish@protonmail.com>
2021-04-05 22:41:31 +0000

    add rating query

---
+++ db/queries/avgRating.js
@@ -0,0 +1,19 @@
+const { db } = require('../server.js');
+
+const getRating = function(resource_id) {
+  const queryString= `
+  SELECT resource.title,
+  ROUND(AVG(rating)2) as average_rating
+  FROM resource
+  JOIN user_id ON resources.id = user_id.resource_id
+  WHERE resource_id = $1 AND rating != 0
+  GROUP BY resource.title;`;
+  return db
+  .query(queryString, [resource_id])
+  .then((result) => {
+    return result.rows;
+  })
+  .catch((err) => console.error(err.stack));
+};
+
+module.exports = { getRating }
