0d2d0e4b266d423f59dc648b5f5f741e07b7d614
vagrant <bbrackish@protonmail.com>
2021-04-07 21:28:34 +0000

    finish comment box

--- routes/comment-router.js
+++ routes/comment-router.js
@@ -6,33 +6,48 @@ const router  = express.Router();
 
 const commentRoutes = (db) => {
 
-  router.get("/", (req, res) => {
+  router.get("/:id", (req, res) => {
     const cookie = req.cookies.user_id;
     if(!cookie) {
       res.redirect('/login')
     }
-    const templateVars = {resource: {}}
-    res.render('comments', templateVars)
-    console.log('&&&&', req.body);
-    console.log('####', res.body);
+    let query = `
+    SELECT * FROM comments
+    WHERE resource_id = $1;`;
+    db.query(query, [req.params.id])
+    .then(response => {
+      console.log(response.rows, 'flagg')
+      templateVars = { comments: response.rows}
+          res.render('comments', templateVars);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      // res.render('comments')
+    })
+    // const templateVars = {resource: {}}
+    // res.render('comments', templateVars)
+    // console.log('&&&&', req.body);
+    // console.log('####', res.body);
   })
 
 
   router.post("/", (req, res) => {
-    console.log(req.cookies.user_id);
+    // console.log(req.cookies.user_id);
     const cookie = req.cookies.user_id;
     if (!cookie) {
       res.redirect('/login')
     }
-    console.log('*****', req.body)
-    console.log('^^^^^',res.body)
+    // console.log('*****', req.body)
+    // console.log('^^^^^',res.body)
     let query = `
       INSERT INTO comments (resource_id, user_id, comment)
       VALUES ($1, $2, $3);`;
     console.log(query, req.cookies.user_id);
     db.query(query, [req.body.res_id, req.body.u_id, req.body.comments])
       .then(response => {
-        res.redirect('/resources/comments');
+        res.redirect(`/resources/comments/${req.body.res_id}`);
       });
   });
 
